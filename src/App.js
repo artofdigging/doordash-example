6ddc63f5dac0f9c2b25c31c78d9758eefb7f333d
Christopher Norman <chris@artofdigging.com>
2021-12-09 17:31:44 -0800

    Adding final component

--- src/App.js
+++ src/App.js
@@ -9,27 +9,20 @@ import BlackOverlay from './components/BlackOverlay';
 import { useState } from 'react';
 
 function App() {
-
   const [isVisable, setVisability] = useState(false);
-  const [isCollapsed, setCollapsed] = useState(false);
-
   return (
     <div className="App">
 
-      <div className="App-header">
+      <div className="App-header" style={{color: isVisable ? '#ffffff' : '#000000'}}>
         <h1>Building a `BottomSheet` component</h1>
         <p>Click below to show the BottomSheet and play with it's behavior!</p>
         <Button text="Toggle BottomSheet" clickFunc={() => {setVisability(!isVisable)}}/>
         <Button text="Toggle Debug Information" clickFunc={() => console.log('Debug')}/>
       </div>
       
-
-
-
       {isVisable && <BlackOverlay />} 
       <BottomSheet 
         isVisable={isVisable}
-        isCollapsed={isCollapsed}
         close={() => {setVisability(!isVisable)}}
       >
         <TextExample />
--- src/components/BottomSheet/index.js
+++ src/components/BottomSheet/index.js
@@ -7,24 +7,45 @@ import BottomSheetHeader from '../BottomSheetHeader';
 
 
 const BottomSheet = (props) => {
-  const transform = useSpring({config: {tension:210, friction:20 },  transform : props.isVisable ? 'translateY(50%)' : 'translateY(100%)' });
+  const ref = react.useRef();
+  const [isCollapsed, setCollapsed] = useState(true);
 
-  const bind = useDrag(({ down, movement: [mx, my] }) => {
-    console.log(down,my)
-    
+  const transform = useSpring({
+    config:{
+      tension:210,
+      friction:20
+    },
+    from: { transform: 'translateY(100vh)'},
+    to: {
+      transform : (props.isVisable ? 'translateY(50vh)' : 'translateY(100vh)')
+    }
   })
+  const transform2 = useSpring({
+    config:{
+      tension:210,
+      friction:20
+    },
+    from: { transform: 'translateY(100vh)'},
+    to: {
+      transform : (props.isVisable & isCollapsed ? 'translateY(50vh)'  :  (props.isVisable & !isCollapsed) ? 'translateY(0vh)' : 'translateY(100vh)')
+    }
+  })
+  
 
   return (
     <animated.div
       class={ `${styles.bottomSheet}` }
-      style={transform}
-      {...bind()}
-    >
-      <BottomSheetHeader 
-        title="My Custom Title"
-        subTitle="Some more information here"
-        close={props.close}
-      />
+      style={transform, transform2}
+      ref={ref}
+    > 
+      <div onClick={() => setCollapsed(!isCollapsed)}>
+        <BottomSheetHeader 
+          title="My Custom Title"
+          subTitle="Some more information here"
+          close={props.close}
+        />
+      </div>
+      
 
       <div className={styles.bottomSheetContent}>
         {props.children}
